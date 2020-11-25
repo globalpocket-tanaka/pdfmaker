5b0157e619c483b5f7654a7dc4c6e2acffc5038f
田中悟志 <s_tanaka@be-i.net>
2020-11-25 23:23:50 +0900

    処理を実装

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,35 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.rirekisho = functions.https.onRequest((request, response) => {
+    functions.logger.info("rirekisho Start!", {structuredData: true});
+
+    ////////////////////////////////////////////
+    // テンプレート処理
+    ////////////////////////////////////////////
+    let html = request.body['template'];
+    let param = request.body['param'];
+    html = mustache.render(html, param);  
+
+    ////////////////////////////////////////////
+    // ヘッドレスChromeでPDF作成
+    ////////////////////////////////////////////
+    let option = request.body['option'];
+    option.printBackground = true;
+    puppeteer.launch({
+        // headless: false,
+        slowMo: 300,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    }).then(async (browser) => {
+        ////////////////////////////////////////////
+        // 完了時
+        ////////////////////////////////////////////
+        const page = await browser.newPage();
+        await page.setContent(html);
+        // PDF作成処理
+        let result = await page.pdf(option);
+        browser.close();
+        response.send(result);
+        functions.logger.info("rirekisho Finish!", {structuredData: true});
+    });
+});
